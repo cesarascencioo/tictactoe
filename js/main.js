93a1edbb02aad1df724af4cbdde4e7eabeef6e7f
Sandro Sanchez <sandropucp@gmail.com>
2016-11-06 22:20:40 -0500

    Update main.js
    You need to fix the last two options

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     var turnCount = 0;
 
     var cells = $('#board').find('td');
+
     var cell1 = $('#board').find('#1');
     var cell2 = $('#board').find('#2');
     var cell3 = $('#board').find('#3');
@@ -15,8 +16,6 @@ $(document).ready(function () {
     var board = new Array(9);
 
     var buttonNewGame = $('#board').find('#btnNewGame').on('click', function () {
-
-
         cell1.text('');
         cell2.text('');
         cell3.text('');
@@ -42,12 +41,12 @@ $(document).ready(function () {
     cells.on('click', function () {
         if (turnCount % 2 === 0) {
             //$(this).text('X');
-            var randomNumber = 1;
-            if (randomNumber == 1) {
-                cell1.text('X');
-            }
+            //var randomNumber = 1;
+            //if (randomNumber == 1) {
+              //  cell1.text('X');
+            //}
 
-
+            $(this).text('X');
             checkVictory('X');
         } else {
             $(this).text('O');
@@ -57,50 +56,48 @@ $(document).ready(function () {
     });
 
     function checkVictory(player) {
-
-
-        if (cell1 !== '') {
-            if (cell1 == cell2 && cell1 == cell3) {
+        if (cell1.text() !== '') {
+            if (cell1.text() == cell2.text() && cell1.text() == cell3.text()) {
                 alert('Game over! ' + player + ' is the winner!');
             }
         }
 
-        if (cell4 !== '') {
+        if (cell4.text() !== '') {
 
-            if (cell4 == cell5 && cell4 == cell6) {
+            if (cell4.text() == cell5.text() && cell4.text() == cell6.text()) {
                 alert('Game over! ' + player + 'is the winner!');
 
             }
         }
 
-        if (cell7 !== '') {
+        if (cell7.text() !== '') {
 
-            if (cell7 == cell8 && cell7 == cell9) {
+            if (cell7.text() == cell8.text() && cell7.text() == cell9.text()) {
                 alert('Game over! ' + player + 'is the winner!');
 
             }
         }
 
-        if (cell1 !== '') {
+        if (cell1.text() !== '') {
 
-            if (cell1 == cell4 && cell1 == cell7) {
+            if (cell1.text() == cell4.text() && cell1.text() == cell7.text()) {
                 alert('Game over! ' + player + 'is the winner!');
 
             }
         }
 
-        if (cell2 !== '') {
+        if (cell2.text() !== '') {
 
-            if (cell2 == cell5 && cell2 == cell8) {
+            if (cell2.text() == cell5.text() && cell2.text() == cell8.text()) {
                 alert('Game over! ' + player + 'is the winner!');
 
             }
         }
 
 
-        if (cell3 !== '') {
+        if (cell3.text() !== '') {
 
-            if (cell3 == cell6 && cell3 == cell9) {
+            if (cell3.text() == cell6.text() && cell3.text() == cell9.text()) {
                 alert('Game over! ' + player + 'is the winner!');
 
             }
